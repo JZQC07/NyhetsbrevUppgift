d3dd504f1c3a774d91f99c6a4d46c4b7fd141ffd
Jonatan.S <jonatan_schultz@hotmail.com>
2020-08-20 15:44:35 +0200

    Fully working frontend

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    }
+  }
+
+
+  componentDidMount() {
+    fetch('http://localhost:3000/users')
+      .then(response => response.json())
+      .then(res => {
+        if (res.data) {
+          console.log(res.data);
+          this.setState({ users: [...this.state.users, ...res.data] })
+        }
+      })
+  }
+
+  renderUsers() {
+
+    if (this.state.users.length <= 0) {
+      return <div>Loading..</div>
+    }
+    else {
+      return this.state.users.map((val, key) => {
+        return <div key={key}>{val.userName} | {val.userEmail}
+
+        </div>;
+      })
+    }
+
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        {this.renderUsers()}
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
